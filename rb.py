9e67e7de88f25621293bbde8ebca8aed59e1cfaa
Sderftg2 <231FA04F37@gmail.com>
2025-01-22 17:35:27 +0530

    Update rb.py

--- rb.py
+++ rb.py
@@ -3,8 +3,8 @@ import datetime
 from telegram import Update
 from telegram.ext import Application, CommandHandler, CallbackContext
 
-TELEGRAM_BOT_TOKEN = '7627929224:AAH3SJb9rV6SiPocUivuRPBK1ycNM9AoXE4'
-ADMIN_USER_ID = 5759284972
+TELEGRAM_BOT_TOKEN = '7708846625:AAG-0MmKDbkG91aUUc9r7VVYUxAgF1we49c'
+ADMIN_USER_ID = 5142603617
 USERS_FILE = 'users.txt'
 LOG_FILE = 'log.txt'
 attack_in_progress = False
